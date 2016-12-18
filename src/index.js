d3f02c83f183daa6b9e6af232da5f65c36cb4661
Pete Russell <pinkbatz@gmail.com>
2016-12-17 20:39:07 -0800

    Got 'Hi\!' printing on the screen\!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should product some HTML.
+const App = () => {
+  /* The 'fat-arrow' syntax (() => {...}) is almost identical to
+     function() {...}, except that the value of 'this' inside
+     the function scope has a different value. */
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in
+// the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
